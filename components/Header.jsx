645264bbd5b74bcd26ae3292e7abb32096eb5c5d
apariciodevme <apariciodev@proton.me>
2023-06-23 02:13:44 -0300

    calendar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
+
 import {
   MagnifyingGlassIcon,
   GlobeAltIcon,
@@ -8,6 +14,18 @@ import {
 } from "@heroicons/react/24/solid";
 
 const Header = () => {
+  const [searchInput,setSearchInput] = useState('')
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+
+  
+  const selectionRange = {
+    startDate: startDate,
+   endDate: endDate,
+    key:'Selectionn'
+  }
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 p-5 shadow-md md:px-10 bg-neutral-50">
       {/* left nav */}
@@ -24,6 +42,8 @@ const Header = () => {
 
       <div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm ">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value) }
           type="text"
           className="flex-grow pl-5 text-gray-600 bg-transparent outline-none"
           placeholder="Start your search"
@@ -43,6 +63,14 @@ const Header = () => {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+
+      {searchInput && (
+        <div>
+          <DateRangePicker
+          ranges={[selectionRange]}
+          />
+        </div>
+      )}
     </header>
   );
 };
