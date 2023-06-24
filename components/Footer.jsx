1c1e9fc3c2d92aaa76e0114f1ea1a45e8900d6e2
apariciodevme <apariciodev@proton.me>
2023-06-24 13:33:30 -0300

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,40 +1,28 @@
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="bottom-0 grid px-20 text-gray-600 bg-gray-100 gap-y-10 sm:grid-cols-2 md:grid-cols-4 py-14">
-      <div className="space-y-4 text-sm text-gray-800">
-        <h5 className="font-bold uppercase ">About</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+    <footer className="bottom-0 flex flex-col items-center justify-between px-20 space-y-4 text-gray-600 bg-gray-100 lg:flex-row py-14">
+      <div className="flex flex-col items-center gap-4 text-sm font-medium text-gray-800 sm:flex-row">
+        <p className="text-gray-600">© 2023 Airbnb, Inc.</p>
+        <p className="cursor-pointer">Terms</p>
+        <p className="cursor-pointer">Sitemap</p>
+        <p className="cursor-pointer">Privacy</p>
+        <p className="cursor-pointer">Destinations</p>
       </div>
 
-      <div className="space-y-4 text-sm text-gray-800">
-        <h5 className="font-bold uppercase ">About</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+      <div className="flex items-center gap-4 text-sm font-semibold text-gray-900 ">
+        <p className="flex items-center gap-2 ">
+          {" "}
+          <GlobeAltIcon className="w-5 h-5" /> English (US)
+        </p>
+        <p>$ USD</p>
       </div>
 
-      <div className="space-y-4 text-sm text-gray-800">
-        <h5 className="font-bold uppercase ">About</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
-
-      <div className="space-y-4 text-sm text-gray-800">
-        <h5 className="font-bold uppercase ">About</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+      <div className="pt-8 lg:pt-0">
+        <Image src={"/airbnb.png"} width={30} height={30} alt="logo" />
       </div>
     </footer>
   );
